1415f5cee1e3ec4f7be97cd017588b411f2850f0
jrumbolz <157167244+jrumbolz@users.noreply.github.com>
2026-02-27 15:41:36 -0700

    added functions for installing folders

--- Installer/installer.py
+++ Installer/installer.py
@@ -3,12 +3,28 @@ from pathlib import Path
 
 
 #main
+class installer:
 
-appname = 'Money Mentor'
-directory_path = Path(f'C:\{appname}')
+    def __init__(self, dir, foldername, filename, filetype):
+        self.dir = dir,
+        self.foldername = foldername,
+        self.filename = filename,
+        self.filetype = filetype
+    
+    def install_folder(self):
+        directory_path = Path(self.dir)
 
-if directory_path.is_dir():
-    print(f"The directory '{directory_path}' exists.")
-else:
-    print(f"The directory '{directory_path}' does not exist or is a file.")
-    os.makedirs(f'C:\{appname}')
+        if directory_path.is_dir():
+            print(f"The directory '{directory_path}' exists.")
+        else:
+            print(f"The directory '{directory_path}' does not exist or is a file.")
+            os.makedirs(f'{self.dir}{self.foldername}')
+
+    def install_folder(self):
+            directory_path = Path(self.dir)
+
+            if directory_path.is_dir():
+                print(f"The directory '{directory_path}' exists.")
+            else:
+                print(f"The directory '{directory_path}' does not exist or is a file.")
+                os.makedirs(f'{self.dir}{self.filename}')
