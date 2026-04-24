23d2daf1d0e300909d5fbdbd4e1675e4a99ea265
jrumbolz <157167244+jrumbolz@users.noreply.github.com>
2026-04-24 16:00:10 -0600

    folder and file clean up
    deleted some folders and files that i believe weren't ever used

--- Money Mentor/BackEnd/FileStorage/add_update_file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-#main
-def add_file(year, month):
-    print(f'C:\Money Mentor\data{year}\{month}')
-    os.makedirs(f'C:\Money Mentor\data\{year}\{month}')
-
-add_file(2026, 'Febuary')
